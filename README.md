a1a3f1dd6df4aca4a09a4415efb845381bd51077
Conrading <47925027+Conrading@users.noreply.github.com>
2019-08-15 15:53:21 +0200

    init file

---
+++ README.md
@@ -0,0 +1,17 @@
+### Run an ICO
+
+`npm run ico`
+
+### Run nodes
+
+`npm run dev`
+
+`HTTP_PORT=3002 P2P_PORT=5002 PEERS=wc://localhost:5001 npm run dev`
+
+`HTTP_PORT=3003 P2P_PORT=5003 PEERS=wc://localhost:5002 npm run dev`
+
+### Initiate System
+
+1. Send coins to the nodes using ico
+2. Recievers can now send stake transactions to be eligible to be elected as leader
+3. Recievers can now send normal transactions to other nodes
---
+++ chain-util.js
@@ -0,0 +1,33 @@
+const EDDSA = require("elliptic").eddsa;
+const eddsa = new EDDSA("ed25519");
+
+const uuidV1 = require("uuid/v1");
+
+const SHA256 = require("crypto-js/sha256");
+
+class ChainUtil {
+  static genKeyPair(secret) {
+    return eddsa.keyFromSecret(secret);
+  }
+
+  static id() {
+    return uuidV1();
+  }
+
+  static hash(data) {
+    return SHA256(JSON.stringify(data)).toString();
+  }
+
+  static verifySignature(publicKey, signature, dataHash) {
+    var verified;
+    try {
+      verified = eddsa.keyFromPublic(publicKey).verify(dataHash, signature);
+    } catch (error) {
+      console.log("error: %o", error);
+      verified = false;
+    }
+    return verified;
+  }
+}
+
+module.exports = ChainUtil;
